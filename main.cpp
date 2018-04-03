9bd53028e5bb0387ba289bdcb0bb293102ab3762
Jorge <utilizador@MacBook-Pro-de-Utilizador.local>
2018-04-03 15:07:18 +0100

    Fase II

--- main.cpp
+++ main.cpp
@@ -15,12 +15,15 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <regex>
 #include <limits>
-#include <cstdio>
+#include "Eigen/Dense"
+
+using namespace Eigen;
+using namespace std;
 
 class Point
 {
-
 	double x, y, z;
 public:
 
@@ -30,24 +33,17 @@ public:
 		this->z = z;
 	}
 
-	double getX(){
-		return x;
-	}
-
-	double getY(){
-		return y;
-	}
-
-	double getZ(){
-		return z;
-	}
+	double getX(){ return x; }
+	double getY(){ return y; }
+	double getZ(){ return z; }
 };
 
+vector<Point> points;
 
-std::vector<std::string> ficheiros;
-std::vector<Point> pontos;
 float rotateHorizontal = 0;
 float rotateVertical = 0;
+float color1 = 0, color2 = 0, color3 = 0;
+
 int mode = GL_LINE;
 
 void changeSize(int w, int h) {
@@ -66,7 +62,7 @@ void changeSize(int w, int h) {
 	glLoadIdentity();
 	
 	// Set the viewport to be the entire window
-    glViewport(0, 0, w, h);
+    	glViewport(0, 0, w, h);
 
 	// Set perspective
 	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
@@ -75,42 +71,144 @@ void changeSize(int w, int h) {
 	glMatrixMode(GL_MODELVIEW);
 }
 
-void loadPointsToMemory(std::string fileName){
-	std::ifstream input(fileName.c_str());
-	std::string line;
+void loadPointsToMemory( string fileName, Matrix4d matrix ){
+	ifstream input( fileName.c_str() );
+	string line;
+	regex numberReg("-?[0-9]+\\.?[0-9]*");
 	double x, y, z;
 	int num;
-	if(input){
-		while(std::getline(input,line)){
-			std::sscanf(line.c_str(),"%lf;%lf;%lf",&x,&y,&z);
-			pontos.push_back(Point(x,y,z));
+	regex_iterator<string::iterator> end;
+
+	if ( input != NULL ) {
+		while ( getline( input,line ) ) { 
+			num = 0;
+			regex_iterator<string::iterator> it ( line.begin(), line.end(), numberReg);
+			while (it != end) {
+				switch (num) {
+					case 0:
+						x = stod(it->str());
+						break;
+					case 1:
+						y = stod(it->str());
+						break;
+					case 2:
+						z = stod(it->str());
+						break;
+					default:
+						break;
+				}
+				num++;
+				it++;
+			}
+			
+			Vector4d coord;
+			coord << x, y, z, 1;
+
+			coord = matrix * coord;
+			points.push_back( Point( coord(0),coord(1),coord(2) ));
 		}
 	}
 	else
 		printf("Unable to open: %s\n",fileName.c_str());
 }
 
-void loadFiguresFromXML(const char * xmlFile){
-	TiXmlDocument doc(xmlFile);
+Matrix4d translateMatrix( TiXmlElement * elem, Matrix4d m ) {
+	double x, y, z;
+	Matrix4d t(4,4);
 
-	bool loaded = doc.LoadFile();
-	if(loaded){
+	elem->Attribute( "X",&x );
+	elem->Attribute( "Y",&y );
+	elem->Attribute( "Z",&z );
 		
-		for(TiXmlElement * e = doc.FirstChildElement("scene")->FirstChildElement("model"); e != NULL; e = e->NextSiblingElement("model")){
-			ficheiros.push_back(e->Attribute("file"));
-		}
-		for (int i = 0; i < ficheiros.size();i++){
-			loadPointsToMemory(ficheiros.at(i));
-		}
-	}
-	else{
-	 	printf("Failed to load %s \n.",xmlFile);
-	}
+	t <<	1, 0, 0, x,
+		0, 1, 0, y,
+		0, 0, 1, z,
+		0, 0, 0, 1;
 	
+	return t * m;	
 }
 
+Matrix4d rotateMatrix( TiXmlElement * elem, Matrix4d m ) {
+	double angle, x, y, z, c, s;
+	Matrix4d r(4,4);
+	
+	elem->Attribute( "angle",&angle );
+	elem->Attribute( "axisX",&x );
+	elem->Attribute( "axisY",&y );
+	elem->Attribute( "axisZ",&z );
 
+	c = cos( angle * (M_PI / 180) );
+	s = sin( angle * (M_PI / 180) );
 
+	r <<	( x * x * (1 - c) + c ),	( x * y * (1 - c) - z * s ),	( x * z * (1 - c) + y * s ),	0,
+		( y * x * (1 - c) + z * s ),	( y * y * (1 - c) + c),		( y * z * (1 - c) - x * s ),	0,
+		( z * x * (1 - c) - y * s ),	( z * y * (1 - c) + x * s ),	( z * z * (1 - c) + c ),	0,
+		0,				0,				0,				1; 
+	
+	return r * m;	
+}
+
+Matrix4d scaleMatrix( TiXmlElement * elem, Matrix4d m ) {
+	double x, y, z;
+	Matrix4d s(4,4);
+
+	elem->Attribute( "X",&x );
+	elem->Attribute( "Y",&y );
+	elem->Attribute( "Z",&z );
+
+	s <<	x, 0, 0, 0,
+		0, y, 0, 0,
+		0, 0, z, 0,
+		0, 0, 0, 1;
+	
+	return s * m;
+}
+
+void loadGroupFromXML( TiXmlElement * group, Matrix4d m ) {
+	TiXmlElement * elem;
+	double angle, x, y, z;
+	int translateFlag = 0, rotateFlag = 0, scaleFlag = 0;
+	
+	
+	for ( elem = group->FirstChildElement(); elem != NULL; elem = elem->NextSiblingElement() ) {
+		if ( strcmp( elem->Value(),"translate" ) == 0 && translateFlag == 0 ) {
+			m = translateMatrix( elem,m );
+			translateFlag++;
+		} 
+		else if ( strcmp( elem->Value(),"rotate" ) == 0 && rotateFlag == 0 ) {
+			m = rotateMatrix( elem,m );
+			rotateFlag++;
+		}
+		else if ( strcmp( elem->Value(),"scale" ) == 0 && scaleFlag == 0 ) {
+			m = scaleMatrix( elem,m );
+			scaleFlag++;
+		}
+	}
+
+	// Models
+	elem = group->FirstChildElement("models");
+	if( elem != NULL ) {
+		for( elem = elem->FirstChildElement("model"); elem != NULL; elem = elem->NextSiblingElement("model")){
+			loadPointsToMemory( elem->Attribute( "file" ),m );
+		}	
+	}
+
+	// Other groups
+	for( elem = group->FirstChildElement("group"); elem != NULL; elem = elem->NextSiblingElement("group") )
+		loadGroupFromXML( elem,m );
+}
+
+void loadFiguresFromXML(const char * xmlFile){
+	TiXmlDocument doc(xmlFile);
+	Matrix4d m = Matrix4d::Identity(4,4);
+
+	bool loaded = doc.LoadFile();
+	if(loaded)
+		for( TiXmlElement * group = doc.FirstChildElement("scene")->FirstChildElement("group"); group != NULL; group = group->NextSiblingElement("group") )
+			loadGroupFromXML( group,m );
+	else
+	 	printf("Failed to load %s \n.",xmlFile);
+}
 
 void handleKeyboardSpecialEvent(int key_code,int x, int y){
 	switch(key_code){
@@ -151,6 +249,24 @@ void handleKeyboardEvent(unsigned char key, int x, int y){
 		case 'p':
 			mode = GL_POINT;
 			break;
+		case '1':
+			if (color1 < 1.0) color1 += 0.1;
+			break; 
+		case '2':
+			if (color2 < 1.0) color2 += 0.1;
+			break; 
+		case '3':
+			if (color3 < 1.0) color3 += 0.1;
+			break; 
+		case '4':
+			if (color1 > 0.0) color1 -= 0.1;
+			break; 
+		case '5':
+			if (color2 > 0.0) color2 -= 0.1;
+			break; 
+		case '6':
+			if (color3 > 0.0) color3 -= 0.1;
+			break; 
 		default:
 			break;
 	}
@@ -160,19 +276,21 @@ void handleKeyboardEvent(unsigned char key, int x, int y){
 void renderScene(void) {
 
 	// clear buffers
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	// set the camera
 	glLoadIdentity();
-	gluLookAt(6.0 * cos(rotateVertical) * sin(rotateHorizontal),6.0 * sin(rotateVertical),6.0*cos(rotateVertical)*cos(rotateHorizontal), 
+	gluLookAt(250.0 * cos(rotateVertical) * sin(rotateHorizontal),250.0 * sin(rotateVertical),250.0 * cos(rotateVertical)*cos(rotateHorizontal), 
 		      0.0,0.0,0.0,
 			  0.0f,1.0f,0.0f);
 
 	glPolygonMode(GL_FRONT,mode);
 
-	//Draw triangles
-	std::vector<Point>::iterator it;
-	for(it = pontos.begin();it != pontos.end(); it++){
+	vector<Point>::iterator it;
+
+	for( it = points.begin() ; it != points.end() ; it++ ) {
+		glColor3f(color1, color2, color3 );	
 		glBegin(GL_TRIANGLES);
 			glVertex3f(it->getX(),it->getY(),it->getZ());
 			it++;
@@ -187,30 +305,33 @@ void renderScene(void) {
 }
 
 int main(int argc, char **argv) {
+
 	printf("Use arrows to rotate image.\nTo change filling:\nf-> FILL\nl-> LINE\np->POINT\n");
-// init GLUT and the window
+
+	// init GLUT and the window
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
 	glutInitWindowPosition(100,100);
 	glutInitWindowSize(800,800);
 	glutCreateWindow("CG@DI-UM");
 		
-// Required callback registry 
+	// Required callback registry 
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(changeSize);
 	
-// Callback registration for keyboard processing
+	// Callback registration for keyboard processing
 	glutKeyboardFunc(handleKeyboardEvent);
 	glutSpecialFunc(handleKeyboardSpecialEvent);
 
-//  OpenGL settings
+	//  OpenGL settings
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
-//Load triangles from .3d files to be drawn
-//Verificar índice do argv
+
+	//Load triangles from .3d files to be drawn
+	//Verificar índice do argv
 	loadFiguresFromXML(argv[1]);
 
-// enter GLUT's main cycle
+	// enter GLUT's main cycle
 	glutMainLoop();
 	
 	return 1;
