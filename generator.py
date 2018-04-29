f6d166e336a70a239676f7e4fbf97b6adbc9a60a
luismeruje <luismeruje@gmail.com>
2018-04-29 02:28:30 +0100

    Generator consegue produzir figuras através de superfícies de bezier

--- generator.py
+++ generator.py
@@ -12,7 +12,8 @@ def plane(size, fileName):
 	file.write("{:.10f};0;{:.10f}\n".format(size/2,-size/2))
 	file.write("{:.10f};0;{:.10f}\n".format(-size/2,-size/2))
 	file.write("{:.10f};0;{:.10f}\n".format(-size/2,size/2))
-	return;
+	file.close()
+	return
 
 
 
@@ -82,7 +83,8 @@ def box(dim_X, dim_Y, dim_Z, divisions, fileName):
 			file.write("{};{};{}\n".format(-dx + x * deltax, -dy, -dz + (z+1) * deltaz))
 			file.write("{};{};{}\n".format(-dx + (x+1) * deltax, -dy, -dz + z * deltaz))
 			file.write("{};{};{}\n".format(-dx + (x+1) * deltax, -dy, -dz + (z+1) * deltaz))
-	return;
+	file.close()
+	return
 
 
 
@@ -151,7 +153,9 @@ def sphere(radius, slices, stacks, fileName):
 			file.write("0.0;{:.10f};0.0\n".format(radius))
 			file.write("{:.10f};{:.10f};0.0\n".format( stackR    , stackh ))
 			file.write("{:.10f};{:.10f};{:.10f}\n".format( stackR*math.cos(y*alfa)    , stackh , stackR*math.sin(y*alfa)  ))
-	return;
+	
+	file.close()
+	return
 
 
 
@@ -216,8 +220,8 @@ def cone(radius, height, slices, stacks, fileName):
 			file.write( "{:.10f};{:.10f};{:.10f}\n".format( stackR*math.cos(x*alfa)    , stackh , stackR*math.sin(x*alfa)  ))
 
 
-
-	return;
+	file.close()
+	return
 
 
 def disc(radiusIn, radiusOut, slices, fileName):
@@ -249,15 +253,171 @@ def disc(radiusIn, radiusOut, slices, fileName):
 		file.write("{:.10f};0.0;{:.10f}\n".format( radiusIn * math.cos( alfa * i ) , radiusIn * math.sin( alfa * i )))
 		file.write("{:.10f};0.0;{:.10f}\n".format( radiusIn * math.cos( alfa * (i+1)) , radiusIn * math.sin( alfa * (i+1) )))
 		file.write("{:.10f};0.0;{:.10f}\n".format( radiusOut * math.cos( alfa * (i+1) ) , radiusOut * math.sin( alfa * (i+1) )))
-
-	return;
-
-
+	file.close()
+	return
 
 
+def bezier(inFileName,tesselation,outFileName):
+	patchesReferencePoints = list()
+	points = list()
+	referencePoints = list()
+
+	outFile = open(outFileName,"w")
+
+	with open(inFileName) as inFile:
+		lines = inFile.readlines()
+	if len(lines) < 7:
+		print "Invalid patch file\n"
+	else:
+		lines = [x.strip() for x in lines]
+		for i in range(1,int(lines[0])+1):
+			patchesReferencePoints.append(lines[i])
+		for i in range(int(lines[0])+2,len(lines)):
+			points.append(lines[i])
+		for p in patchesReferencePoints:
+			writePatchPointsToFile(p,points,tesselation,outFile)
+
+	inFile.close()
+	outFile.close()
+	return
+
+def writePatchPointsToFile(patchReferencePoints,points,tesselation,outFile):
+	curveReferencePoints = list()
+	curvePoints = list()
+	previousCurvePoints = list()
+	for i in range(0,tesselation+1):
+		t = float(i) / float(tesselation)
+		if i == 0:
+			curveReferencePoints = interpolateCurveReferencePoints(patchReferencePoints,points,t)
+			previousCurvePoints = determineCurvePoints(curveReferencePoints,tesselation)
+		else:
+			curveReferencePoints = interpolateCurveReferencePoints(patchReferencePoints,points,t)
+			curvePoints = determineCurvePoints(curveReferencePoints,tesselation)
+			writeTrianglesToFile(previousCurvePoints,curvePoints,outFile)
+			previousCurvePoints = curvePoints
+
+	return
+
+def writeTrianglesToFile(line1,line2,outFile):
+	previousPoint1 = list()
+	previousPoint2 = list()
+	point1 = list()
+	point2 = list()
+	i = 0
+
+	for p1,p2 in zip(line1,line2):
+		if i == 0:
+			previousPoint1 = p1
+			previousPoint2 = p2
+			i = 1
+		else:
+			point1 = p1
+			point2 = p2
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(previousPoint1[0],previousPoint1[1],previousPoint1[2]))
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(previousPoint2[0],previousPoint2[1],previousPoint2[2]))
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(point1[0],point1[1],point1[2]))
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(point1[0],point1[1],point1[2]))
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(previousPoint2[0],previousPoint2[1],previousPoint2[2]))
+			outFile.write("{:.10f};{:.10f};{:.10f}\n".format(point2[0],point2[1],point2[2]))
+
+	return
 
 
 
+def interpolateCurveReferencePoints(patchReferencePoints,points,t):
+	interpolatedCurveReferencePoints = list()
+	curveReferencePoints = [None] * 4
+	for i in range(0,4):
+		referencePointsIndexes = patchReferencePoints.split(",")
+		curveReferencePoints[0] = points[int(referencePointsIndexes[0+i*4])]
+		curveReferencePoints[1] = points[int(referencePointsIndexes[1+i*4])]
+		curveReferencePoints[2] = points[int(referencePointsIndexes[2+i*4])]
+		curveReferencePoints[3] = points[int(referencePointsIndexes[3+i*4])]
+		interpolatedCurveReferencePoints.append(pointInBezierCurveFromStrings(curveReferencePoints,t))
+
+	return interpolatedCurveReferencePoints;
+
+def determineCurvePoints(curveReferencePoints,tesselation):
+	points = list()
+
+	for i in range(0,tesselation+1):
+		t = float(i) / float(tesselation)
+		points.append(pointInBezierCurve(curveReferencePoints,t))
+	return points
+
+def pointInBezierCurve(referencePoints,t):
+	P1 = list()
+	P2 = list()
+	P3 = list()
+	P4 = list()
+	point = list()
+
+	k1 = (1-t)**3
+	k2 = 3*(1-t)**(2)*t
+	k3 = 3 * (1-t)*t**2
+	k4 = t**3
+	
+	P1.append(referencePoints[0][0])
+	P1.append(referencePoints[0][1])
+	P1.append(referencePoints[0][2])
+
+	P2.append(referencePoints[1][0])
+	P2.append(referencePoints[1][1])
+	P2.append(referencePoints[1][2])
+
+	P3.append(referencePoints[2][0])
+	P3.append(referencePoints[2][1])
+	P3.append(referencePoints[2][2])
+
+	P4.append(referencePoints[3][0])
+	P4.append(referencePoints[3][1])
+	P4.append(referencePoints[3][2])
+
+	for p1Coord,p2Coord,p3Coord,p4Coord in zip(P1,P2,P3,P4):
+		point.append(k1*p1Coord + k2*p2Coord + k3 * p3Coord + k4 * p4Coord)
+
+	return point
+
+def pointInBezierCurveFromStrings(referencePoints,t):
+	P1 = list()
+	P2 = list()
+	P3 = list()
+	P4 = list()
+	point = list()
+
+	k1 = (1-t)**3
+	k2 = 3*(1-t)**(2)*t
+	k3 = 3 * (1-t)*t**2
+	k4 = t**3
+	
+	values = referencePoints[0].split(",")
+	P1.append(float(values[0]))
+	P1.append(float(values[1]))
+	P1.append(float(values[2]))
+
+	values = referencePoints[1].split(",")
+	P2.append(float(values[0]))
+	P2.append(float(values[1]))
+	P2.append(float(values[2]))
+
+	values = referencePoints[2].split(",")
+	P3.append(float(values[0]))
+	P3.append(float(values[1]))
+	P3.append(float(values[2]))
+
+	values = referencePoints[3].split(",")
+	P4.append(float(values[0]))
+	P4.append(float(values[1]))
+	P4.append(float(values[2]))
+
+	for p1Coord,p2Coord,p3Coord,p4Coord in zip(P1,P2,P3,P4):
+		point.append(k1*p1Coord + k2*p2Coord + k3 * p3Coord + k4 * p4Coord)
+
+	return point
+
+
+
+#TODO: usar switch no argv[1]
 def main(argv):
 	if len(argv) > 2:
 		if argv[1] == "plane":
@@ -287,11 +447,15 @@ def main(argv):
 				disc(float(argv[2]),float(argv[3]),float(argv[4]),argv[5])
 			else:
 				print("ERROR: cone should have 3 additional arguments and output file.\nArguments: radiusIn radiusOut slices  outputFile\n")
-			
+		elif argv[1] == 'bezier':	
+			if len(argv) == 5:
+				bezier(argv[2],int(argv[3]),argv[4]);
+			else:
+				print("ERROR: bezier should have 1 additional argument and output file.\nArgument: tesselation\n")
 		else:
 			print("ERROR: Invalid shape\n")
 	else:
 		print("Usage: ./generator.py shape arguments outputFile\n")
-		return;
+		return
 
 main(sys.argv)
