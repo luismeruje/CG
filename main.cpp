d73315e7811be24f62e46f562564f114bd9aeee7
luismeruje <luismeruje@gmail.com>
2018-05-18 16:06:32 +0100

    Added lights

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+#include <stdlib.h> 
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
@@ -32,9 +32,16 @@ using namespace std::tr1;
 using namespace Eigen;
 using namespace std;
 float *vertexb;
-vector<float> pointsArray;
+float *normalb;
+float *normalv;
+vector<float> changingPoints;
 
-GLuint buffers[2];
+GLuint buffers[4];
+
+int nrOfLights = 0;
+//Four doubles per light
+vector<double> posOrDirOfLights;
+
 
 //WARNING: Princípios de encapsulamento não foram aplicados por ser um programa simples. São passados objetos entre funções e não cópias.
 class Point
@@ -55,34 +62,25 @@ public:
 
 class Rotate
 {
-    float * modelPoints;
+    vector<float> modelPoints;
     int time;
     float * pivot;
     Matrix4d matrix;
-    int nrOfPoints;
 
 public:
     Rotate(int time,shared_ptr<Point> pivot, Matrix4d matrix){
-        this->modelPoints = (float *)malloc(sizeof(float) * 3 * 200000);
         this->time = time;
         this->pivot = (float *)malloc(sizeof(float) * 3);
         this->pivot[0] = pivot->getX();
         this->pivot[1] = pivot->getY();
         this->pivot[2] = pivot->getZ();
         this->matrix = matrix;
-        this->nrOfPoints = 0;
     }
 
-    float * getModelPoints(){return modelPoints;}
+    vector<float> * getModelPoints(){return &modelPoints;}
 	int getTime(){return time;}
 	float*  getPivot(){return pivot;}
 	Matrix4d getMatrix(){return matrix;}
-
-	void incrementNrOfPoints(){
-		nrOfPoints++;
-	}
-
-	int getNrOfPoints(){return nrOfPoints;}
 
 };
 
@@ -93,8 +91,7 @@ int nrOfRotates = 0;
 //vector<Translate> translates;
 float rotateHorizontal = 0;
 float rotateVertical = 0;
-float color1 = 0, color2 = 0, color3 = 0;
-int pointc=0;
+float color1 = 1, color2 = 1, color3 = 1;
 int mode = GL_LINE;
 
 void changeSize(int w, int h) {
@@ -139,7 +136,6 @@ void loadPointsToMemory( string fileName, Matrix4d matrix ) {
 
 			coord = matrix * coord;
 			points.push_back(Point(coord(0), coord(1), coord(2)));
-			pointc++;
 		}
 	}
 }
@@ -149,7 +145,7 @@ void loadPointsToRotate(string fileName){
     string line;
     shared_ptr<Rotate> rotateInstance = rotates.back();
 
-    float * rotatePoints = rotateInstance->getModelPoints();
+    vector<float> * rotatePoints = rotateInstance->getModelPoints();
     double x, y, z;
     int num;
     int pos = 0;
@@ -159,10 +155,9 @@ void loadPointsToRotate(string fileName){
     else {
         while (getline(input, line)) {
             sscanf(line.c_str(), "%lf;%lf;%lf", &x, &y, &z);
-            rotatePoints[pos++] = x;
-            rotatePoints[pos++] = y;
-            rotatePoints[pos++] = z;
-            rotateInstance->incrementNrOfPoints();
+            rotatePoints->push_back(x);
+            rotatePoints->push_back(y);
+            rotatePoints->push_back(z);;
         }
     }
 
@@ -281,6 +276,7 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 		return scalingMatrix * matrix;
 	}
 
+	//TODO: se for passado só x ou só y ou z, é considerado sintace válida, corrigir.
 	void createNewRotate(TiXmlElement *elem, Matrix4d matrix) {
 		double x, y, z;
 		int time;
@@ -332,7 +328,7 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
             	for (elem = elem->FirstChildElement("model"); elem != NULL; elem = elem->NextSiblingElement("model")) {
                 	loadPointsToRotate(elem->Attribute("file"));
                 }
-                specialFlag = 0;
+                //specialFlag = 0;
             }
 		}
 
@@ -342,15 +338,57 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 
 	}
 
-	void loadFiguresFromXML(const char *xmlFile) {
+	void registerLight(TiXmlElement * light){
+		double X=0,Y=0, Z=0;
+		const char * type = NULL;
+
+		type = light->Attribute("type");
+		light->Attribute("X",&X);
+		light->Attribute("Y",&Y);
+		light->Attribute("Z",&Z);
+
+		if(type != NULL){
+			if(!strcmp(type, "POINT")){
+				posOrDirOfLights.push_back(X);
+				posOrDirOfLights.push_back(Y);
+				posOrDirOfLights.push_back(Z);
+				posOrDirOfLights.push_back(1.0);
+
+				nrOfLights++;
+			}
+			else if(!strcmp(type, "DIRECTIONAL")){
+				posOrDirOfLights.push_back(X);
+				posOrDirOfLights.push_back(Y);
+				posOrDirOfLights.push_back(Z);
+				posOrDirOfLights.push_back(0.0);
+
+				nrOfLights++;
+			}
+		}
+
+
+	}
+	
+
+	void loadXML(const char *xmlFile) {
 		TiXmlDocument doc(xmlFile);
 		Matrix4d matrix = Matrix4d::Identity(4, 4);
 
 		bool loaded = doc.LoadFile();
 		if (loaded) {
-			for (TiXmlElement *group = doc.FirstChildElement("scene")->FirstChildElement("group");
-				 group != NULL; group = group->NextSiblingElement("group"))
-				loadGroupFromXML(group, matrix);
+			TiXmlElement * root = doc.FirstChildElement("scene");
+			if(root){
+				TiXmlElement * lights = root->FirstChildElement("lights");
+				if(lights){
+					for(TiXmlElement * light = lights->FirstChildElement("light");
+						light != NULL && nrOfLights < 8;light = light -> NextSiblingElement("light")){
+						registerLight(light);
+					}
+				}
+				for (TiXmlElement *group = root->FirstChildElement("group");
+					 group != NULL; group = group->NextSiblingElement("group"))
+					loadGroupFromXML(group, matrix);
+			}
 		} else
 			printf("Failed to load %s \n.", xmlFile);
 	}
@@ -453,43 +491,84 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 
 	}
 
+	void fillChangingNormalsVBO(){
+		normalv = (float *) malloc(sizeof(float) * changingPoints.size());
+		float v1[3],v2[3];
+		float n[3],norm;
+
+		for(int i = 0; i < changingPoints.size() / 9; i++){
+			v1[0] = changingPoints.at(i*9+3) - changingPoints.at(i*9+0);
+			v1[1] = changingPoints.at(i*9+4) - changingPoints.at(i*9+1);
+			v1[2] = changingPoints.at(i*9+5) - changingPoints.at(i*9+2);
+			v2[0] = changingPoints.at(i*9+6) - changingPoints.at(i*9+0);
+			v2[1] = changingPoints.at(i*9+7) - changingPoints.at(i*9+1);
+			v2[2] = changingPoints.at(i*9+8) - changingPoints.at(i*9+2);
+
+			n[0] = v1[1]*v2[2] - v1[2]*v2[1];
+			n[1] = v1[2]*v2[0] - v1[0]*v2[2];
+			n[2] = v1[0]*v2[1] - v1[1]*v2[0];
+
+			norm = sqrt(pow(n[0],2)+pow(n[1],2)+pow(n[2],2));
+
+			n[0] = n[0] / norm;
+			n[1] = n[1] / norm;
+			n[2] = n[2] / norm;
+
+			normalv[i*9+0] = n[0];
+			normalv[i*9+1] = n[1];
+			normalv[i*9+2] = n[2];
+			normalv[i*9+3] = n[0];
+			normalv[i*9+4] = n[1];
+			normalv[i*9+5] = n[2];
+			normalv[i*9+6] = n[0];
+			normalv[i*9+7] = n[1];
+			normalv[i*9+8] = n[2];
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffers[3]);;
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * changingPoints.size(), normalv, GL_STATIC_DRAW);
+	}
+
 	void drawDynamicRotates(){
 		vector<shared_ptr<Rotate> >::iterator it;
-		float * points;
+		vector<float> * modelPoints;
 		Matrix4d matrix;
-		int pos, time,nrOfPoints,r;
+		int pos, time, r,i;
 		float * pivot;
 		float * point = (float*)malloc(sizeof(float) * 3);
 
 
 		for (r = 0,it = rotates.begin(); it != rotates.end(); it++,r++){
-			points = rotates.at(r)->getModelPoints();
-			nrOfPoints = rotates.at(r)->getNrOfPoints();
+			modelPoints = rotates.at(r)->getModelPoints();
 			time = rotates.at(r)->getTime();
 			matrix = rotates.at(r)->getMatrix();
 			pivot = rotates.at(r)->getPivot();
-
-			for (int i = 0; i < nrOfPoints; i+=1){
-				getRotatePointPosition(points[i*3],points[i * 3+1],points[i * 3+2],time,matrix,pivot,point);
-				pointsArray.push_back(point[0]);
-				pointsArray.push_back(point[1]);
-				pointsArray.push_back(point[2]);
+			int nrOfPoints = modelPoints->size() / 3;
+			for (i = 0; i < nrOfPoints; i+=1){
+				getRotatePointPosition(modelPoints->at(i*3),modelPoints->at(i * 3+1),modelPoints->at(i * 3+2),time,matrix,pivot,point);
+				changingPoints.push_back(point[0]);
+				changingPoints.push_back(point[1]);
+				changingPoints.push_back(point[2]);
 			}
 		}
+		fillChangingNormalsVBO();
 
 		glBindBuffer(GL_ARRAY_BUFFER,buffers[1]);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * pointsArray.size(), &(pointsArray[0]), GL_STATIC_DRAW);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * changingPoints.size(), &(changingPoints[0]), GL_STATIC_DRAW);
 		glVertexPointer(3, GL_FLOAT, 0, 0);
-		glDrawArrays(GL_TRIANGLES,0,pointsArray.size()/3);
+		glBindBuffer(GL_ARRAY_BUFFER,buffers[3]);
+		glNormalPointer(GL_FLOAT, 0, 0);
+		glDrawArrays(GL_TRIANGLES,0,changingPoints.size()/3);
+		changingPoints.clear();
 
-		pointsArray.clear();
 	}
 
 
+	//TODO: sun should be emissive(plus light at center of sun = pretty good looking solar system)
 	void renderScene(void) {
-
+		glPolygonMode(GL_FRONT, mode);
 		// clear buffers
-		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// set the camera
@@ -499,11 +578,31 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 				  0.0, 0.0, 0.0,
 				  0.0f, 1.0f, 0.0f);
 
+		GLfloat amb[4] = {0.2, 0.2, 0.2, 1.0};
+		GLfloat diff[4] = {1.0, 1.0, 1.0, 1.0};
+		GLfloat pos[4];
+		for(int i = 0; i < nrOfLights; i++){
+			pos[0] = posOrDirOfLights.at(i*4+0);
+			pos[1] = posOrDirOfLights.at(i*4+1);
+			pos[2] = posOrDirOfLights.at(i*4+2);
+			pos[3] = posOrDirOfLights.at(i*4+3);
+
+			//DEBUG
+			//printf("lightID: %d\n\tposition: %f,%f,%f,%f\n",GL_LIGHT0+i,posOrDirOfLights.at(i*4+0),posOrDirOfLights.at(i*4+1),posOrDirOfLights.at(i*4+2),posOrDirOfLights.at(i*4+3));
+
+			glLightfv(GL_LIGHT0+i, GL_POSITION, pos);
+			glLightfv(GL_LIGHT0+i, GL_AMBIENT, amb);
+			glLightfv(GL_LIGHT0+i, GL_DIFFUSE, diff);
+		}
+
 		drawDynamicRotates();
 
 		glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
 		glVertexPointer(3, GL_FLOAT, 0, 0);
-		glDrawArrays(GL_TRIANGLES, 0, pointc);
+		glBindBuffer(GL_ARRAY_BUFFER, buffers[2]);
+		glNormalPointer(GL_FLOAT, 0, 0);
+		//TODO: Vector com os materials, faz-se draw para os intervalos de cada material, definir diffuse e ambient por defeito(iguais aos default do openGL)
+		glDrawArrays(GL_TRIANGLES, 0, points.size());
 
 		//drawDynamicTranslates
 
@@ -512,11 +611,51 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 
 
 	}
-	void fillConstantVBO() {
+
+	void fillConstantNormalsVBO(){
+		normalb = (float *) malloc(sizeof(float) * points.size() * 3);
+		float v1[3],v2[3];
+		float n[3],norm;
+
+		for(int i = 0; i < points.size() / 3; i++){
+			v1[0] = vertexb[i*9+3] - vertexb[i*9+0];
+			v1[1] = vertexb[i*9+4] - vertexb[i*9+1];
+			v1[2] = vertexb[i*9+5] - vertexb[i*9+2];
+			v2[0] = vertexb[i*9+6] - vertexb[i*9+0];
+			v2[1] = vertexb[i*9+7] - vertexb[i*9+1];
+			v2[2] = vertexb[i*9+8] - vertexb[i*9+2];
+
+			n[0] = v1[1]*v2[2] - v1[2]*v2[1];
+			n[1] = v1[2]*v2[0] - v1[0]*v2[2];
+			n[2] = v1[0]*v2[1] - v1[1]*v2[0];
+
+			norm = sqrt(pow(n[0],2)+pow(n[1],2)+pow(n[2],2));
+
+			n[0] = n[0] / norm;
+			n[1] = n[1] / norm;
+			n[2] = n[2] / norm;
+
+			normalb[i*9+0] = n[0];
+			normalb[i*9+1] = n[1];
+			normalb[i*9+2] = n[2];
+			normalb[i*9+3] = n[0];
+			normalb[i*9+4] = n[1];
+			normalb[i*9+5] = n[2];
+			normalb[i*9+6] = n[0];
+			normalb[i*9+7] = n[1];
+			normalb[i*9+8] = n[2];
+		}
+
+		glBindBuffer(GL_ARRAY_BUFFER, buffers[2]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size() * 3, normalb, GL_STATIC_DRAW);
+	}
+
+	void fillConstantVBOs() {
 
 		vector<Point>::iterator it;
 		glColor3f(color1, color2, color3);
-		vertexb = (float *) malloc(sizeof(float) * pointc * 3);
+		vertexb = (float *) malloc(sizeof(float) * points.size() * 3);
+		
 		int pos = 0;
 		for (it = points.begin(); it != points.end(); it++) {
 			vertexb[pos++] = (it->getX());
@@ -529,13 +668,15 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
                 glVertex3f(it->getX(),it->getY(),it->getZ());
                 it++;
                 glVertex3f(it->getX(),it->getY(),it->getZ());
-             */
+             */		
 
 		}
 
+		
 		glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);;
-		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * pointc * 3, vertexb, GL_STATIC_DRAW);
-		//free(vertexb);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(float) * points.size() * 3, vertexb, GL_STATIC_DRAW);
+
+		fillConstantNormalsVBO();
 
 	}
 	void initGL() {
@@ -543,8 +684,26 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 		glEnable(GL_DEPTH_TEST);
 		glEnable(GL_CULL_FACE);
 		glEnableClientState(GL_VERTEX_ARRAY);
-		glGenBuffers(2, buffers);
-		glPolygonMode(GL_FRONT, mode);
+		glEnableClientState(GL_NORMAL_ARRAY);
+		glGenBuffers(4, buffers);
+
+		//DEBUG
+		float red[4] = {0.8f, 0.2f, 0.2f, 1.0f};
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, red);
+
+		
+
+		if(nrOfLights > 0)
+			glEnable(GL_LIGHTING);
+
+		for(int i = 0; i < nrOfLights; i++){
+			glEnable(GL_LIGHT0+i);
+		}
+
+		
+
+
+		
 
 	}
 
@@ -574,18 +733,21 @@ Matrix4d  translateMatrixwtime(TiXmlElement * elem, Matrix4d matrix, Point* pont
 #endif
 
 
-		//Load triangles from .3d files to be drawn
-		loadFiguresFromXML(argv[1]);
+		//Load all elements from xml file
+		loadXML(argv[1]);
+
+		for(int i = 0; i < nrOfLights; i++){
+			printf("Light %d: (%f,%f,%f,%f)\n",i,posOrDirOfLights.at(i*4+0),posOrDirOfLights.at(i*4+1),posOrDirOfLights.at(i*4+2),posOrDirOfLights.at(i*4+3));
+		}
 
 		//Initialize OpenGL
 		initGL();
 
-		//Fill buffer[0] with static points
-		fillConstantVBO();
+		//Fill buffer[0] and buffer[2] with static vectors and respective normals
+		fillConstantVBOs();
 
 		// enter GLUT's main cycle
 		glutMainLoop();
-
 
 		return 1;
 	}
